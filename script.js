02da235d3b7936e40d1b97bedfe8a64379dc6112
DanPrl <danielpereira-oliveira@outlook.com>
2021-10-15 12:04:21 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+// function validateForm() {
+//     var rute = login_form.rute;
+//     if(rute.value == '') {
+//        window.alert("Email não informado");
+//     }
+//       return false;
+//     }
