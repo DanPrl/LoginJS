30b65b3c85ca80574cf36ffc50ac93179d30343d
Daniel Pereira <65868049+DanPrl@users.noreply.github.com>
2021-10-15 12:08:46 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-
-// function validateForm() {
-//     var rute = login_form.rute;
-//     if(rute.value == '') {
-//        window.alert("Email não informado");
-//     }
-//       return false;
-//     }
